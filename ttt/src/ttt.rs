f4730938355004803a6d488901789516de29ec1e
Ryan1729 <Ryan1729@gmail.com>
2020-06-14 15:01:01 -0600

    try removing the addition in case overflow checks were what was being inserted.

--- ttt/src/ttt.rs
+++ ttt/src/ttt.rs
@@ -1,5 +1,5 @@
 pub fn main() -> i32 {
-    2 + 2
+    4
 }
 
 #[cfg(test)]
@@ -7,12 +7,6 @@ mod tests {
     use super::*;
     #[test]
     fn it_works() {
-        assert_eq!(main(), 4);
-    }
-
-    #[test]
-    fn it_works2() {
-        it_works();
         assert_eq!(main(), 4);
     }
 }
