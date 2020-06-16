8113815fa9a5645315a7918e8a47dc68bb5464c8
Ryan1729 <Ryan1729@gmail.com>
2020-06-15 22:04:57 -0600

    add the board and printing thereof, while retaining line and function coverage

--- src/main.rs
+++ src/main.rs
@@ -13,6 +13,14 @@ fn main() -> io::Result<()> {
     run(input, output)    
 }
 
+#[derive(Clone, Copy)]
+enum Space {
+    Blank,
+    X,
+    O,
+}
+use Space::*;
+
 fn run<R, W>(mut reader: R, mut writer: W) -> io::Result<()>
 where
     R: BufRead,
@@ -24,10 +32,71 @@ where
     }
     use State::*;
 
-    let mut state = Game;
+    const LENGTH: usize = 9;
+    type Board = [Space; LENGTH];
 
+    let mut board: Board = [Blank; LENGTH];
+    let mut state = Game;
     let mut buffer = String::new();
+
+    macro_rules! space_to_char {
+        ($index: literal) => {{
+            let index = $index;
+            let space = board[$index];
+            match space {
+                Blank => match index {
+                    0 => '1',
+                    1 => '2',
+                    2 => '3',
+                    3 => '4',
+                    4 => '5',
+                    5 => '6',
+                    6 => '7',
+                    7 => '8',
+                    8 => '9',
+                    _ => '?'
+                },
+                X => 'X',
+                O => '0'
+            }
+        }}
+    }
+
+    write!(&mut writer, "Enter 0 at any time to quit.\n\nEnter the number corresponding to the space to place an X there.")?;
+
     loop {
+        match state {
+            Game => {
+                write!(&mut writer, 
+r#"
++-----+
+|{6}|{7}|{8}|
++-----+
+|{3}|{4}|{5}|
++-----+
+|{0}|{1}|{2}|
++-----+
+"#, 
+                    space_to_char!(0),
+                    space_to_char!(1),
+                    space_to_char!(2),
+                    space_to_char!(3),
+                    space_to_char!(4),
+                    space_to_char!(5),
+                    space_to_char!(6),
+                    space_to_char!(7),
+                    space_to_char!(8),
+                )?;
+            }
+            _ => { // ConfirmQuit or invalid enum
+                write!(
+                    &mut writer, 
+                    "Are you sure you want to quit? (Enter 1 for yes).\n"
+                )?;
+            }
+        }
+        writer.flush()?;
+
         reader.read_line(&mut buffer)?;
 
         if let Some(c) = buffer.chars().next() {
@@ -122,6 +191,11 @@ mod tests {
         let mut output = Vec::new();
     
         let _ = run(FailingReader, &mut output);
+    }
+
+    #[test]
+    fn space_is_cloneable() {
+        let _ = Space::Blank.clone();
     }
 
     struct FailingReader;
